5af3983b63615e3d643722c854af2af406d616fb
Vinícius Pires Lopes <pireslopesvinicius@gmail.com>
2026-06-25 21:59:06 -0300

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://viamedmultisaude.com.br',
   compressHTML: true,
+
   build: {
     assets: '_assets',
   },
+
+  integrations: [sitemap()],
 });
